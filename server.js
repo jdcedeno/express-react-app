eaf9d5bebe36eb1cd85ee7287428ff98cfeb1b26
Jose Cedeno <56703919+jdcedeno@users.noreply.github.com>
2021-03-12 19:01:01 -0500

    small fix, begin testing

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const config = require("config");
 
-console.log(config.get("%%%%%% custom-environment-variables.json %%%%%%%%%%"));
+console.log("%%%%%% custom-environment-variables.json %%%%%%%%%%");
 console.log("should be DEFAULT VALUE", config.get("TEST"));
 console.log("should be 5000", config.get("HEROKUPORT"));
 
-console.log(config.get("%%%%%% default.json %%%%%%%%%%"));
+console.log("%%%%%% default.json %%%%%%%%%%");
 console.log("should be DEFAULT VALUE", config.get("defaultTest"));
 console.log("should be 6000", config.get("localPort"));
 
-console.log(config.get("%%%%%% production.json %%%%%%%%%%"));
+console.log("%%%%%% production.json %%%%%%%%%%");
 console.log("should be PRODUCTION VALUE", config.get("defaultTest"));
 console.log("should be 5000 (from .env)", config.get("HEROKUPORT"));
