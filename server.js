3951349aaee01bac1f1846eb6eadbe71909fd980
Jose Cedeno <56703919+jdcedeno@users.noreply.github.com>
2021-03-12 19:25:25 -0500

    test(passed): dotenv can access environment variables in .env file;

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 const config = require("config");
 
 console.log("%%%%%% custom-environment-variables.json %%%%%%%%%%");
-console.log("should be DEFAULT VALUE", config.get("TEST"));
-console.log("should be 5000", config.get("HEROKUPORT"));
+console.log("should be HELLO_WORLD", config.get("TEST"));
+console.log("should be 6000", config.get("HEROKUPORT"));
 
-console.log("%%%%%% default.json %%%%%%%%%%");
-console.log("should be DEFAULT VALUE", config.get("defaultTest"));
-console.log("should be 6000", config.get("localPort"));
-
-console.log("%%%%%% production.json %%%%%%%%%%");
-console.log("should be PRODUCTION VALUE", config.get("defaultTest"));
-console.log("should be 5000 (from .env)", config.get("HEROKUPORT"));
+if (process.env.NODE_ENV === "production") {
+    console.log("%%%%%% production.json %%%%%%%%%%");
+    console.log("should be PRODUCTION VALUE", config.get("defaultTest"));
+    console.log("should be 6000 (from .env)", config.get("HEROKUPORT"));
+} else {
+    console.log("%%%%%% default.json %%%%%%%%%%");
+    console.log("should be DEFAULT VALUE", config.get("defaultTest"));
+    console.log("should be 7000", config.get("localPort"));
+}
